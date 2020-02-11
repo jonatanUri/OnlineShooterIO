d7188d1f7f4e7ec75b61a81cc291fca7fbe67cbb
Jonatán Uri <urijoni@gmail.com>
2020-02-11 10:23:14 +0100

    Server now runs, shows local, public ip when ran

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+const publicIp = require('public-ip');
+const localIp = require("ip");
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/client/index.html');
+})
+app.use('/client', express.static(__dirname + '/client'));
+serv.listen(2000);
+
+(async () => {
+    console.log("Server runs on: " + await publicIp.v4() + ":2000 (public)");
+    console.log("Or: " + localIp.address() + ":2000 (local)")
+})();
