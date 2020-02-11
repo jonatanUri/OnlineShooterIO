4f7af5bcc1681db2a502d05ffbcd7e5470a03da3
Jonatán Uri <urijoni@gmail.com>
2020-02-11 10:30:02 +0100

    Client IO connection

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const localIp = require("ip");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
-})
+});
 app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
 
@@ -14,3 +14,8 @@ serv.listen(2000);
     console.log("Server runs on: " + await publicIp.v4() + ":2000 (public)");
     console.log("Or: " + localIp.address() + ":2000 (local)")
 })();
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+  console.log('socket connected');
+});
